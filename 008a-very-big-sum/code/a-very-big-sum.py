d63f1c49844017119fa9a875bf01df263776be08
baylierh <higginbothamb@DESKTOP-60TK8HU>
2021-07-13 15:10:51 -0400

    Copied over my simpleArraySum() code

--- 008a-very-big-sum/code/a-very-big-sum.py
+++ 008a-very-big-sum/code/a-very-big-sum.py
@@ -2,4 +2,25 @@
 # Some of those integers may be quite large
 
 def aVeryBigSum(ar):
-    #Code here
+    sum = 0
+    for item in ar:
+        sum += item
+    return sum
+
+def fixArrayInput(ar):
+    new_ar = []
+    for i in ar.split():
+        new_ar.append(int(i))
+    return new_ar
+
+print("Find the sum of an array's elements!")
+print("Separate each element with a space like so: 1 2 3")
+
+# Ask for input
+ar = input("Enter the array: ")
+
+# 'Fix' the input so simpleArraySum() can work with it
+fixed_ar = fixArrayInput(ar)
+
+# Give the fixed array to simpleArraySum and print the result!
+print("Sum:", aVeryBigSum(fixed_ar))
