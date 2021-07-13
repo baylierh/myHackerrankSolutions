9deb73784c614adb51c86a122b7d2acfca0b9dbe
baylierh <higginbothamb@DESKTOP-60TK8HU>
2021-07-13 14:21:17 -0400

    Now takes input, formats it, and returns answer

--- 007simple-array-sum/code/simple-array-sum.py
+++ 007simple-array-sum/code/simple-array-sum.py
@@ -7,3 +7,18 @@ def simpleArraySum(ar):
         sum += item
     return sum
 
+def fixArrayInput(ar):
+    new_ar = []
+    for i in ar.split():
+        new_ar.append(int(i))
+    return new_ar
+     
+
+print("Find the sum of an array's entries!")
+print("(Separate each entry with a space like so: 1 2 3)")
+
+ar = input("Enter the array: ")
+
+fixed_ar = fixArrayInput(ar)
+
+print(simpleArraySum(fixed_ar))
