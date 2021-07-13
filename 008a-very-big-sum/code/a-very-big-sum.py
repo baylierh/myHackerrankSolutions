f5daad0931bc523f22a071f8d703ebf752e01082
baylierh <higginbothamb@DESKTOP-60TK8HU>
2021-07-13 15:16:26 -0400

    Edited the code to put an emphasis on 'large' nums

--- 008a-very-big-sum/code/a-very-big-sum.py
+++ 008a-very-big-sum/code/a-very-big-sum.py
@@ -1,12 +1,16 @@
 # Calculate and print the sum of the elements in an array
 # Some of those integers may be quite large
 
+# aVeryBigSum takes an array as input and adds each element
+#  of it to the variable "sum" which is returned at the end
 def aVeryBigSum(ar):
     sum = 0
     for item in ar:
         sum += item
     return sum
 
+# fixArrayInput takes an array that is in string form
+# (ex: 10001 10002 10003) and turns it into a list of integers
 def fixArrayInput(ar):
     new_ar = []
     for i in ar.split():
@@ -14,7 +18,8 @@ def fixArrayInput(ar):
     return new_ar
 
 print("Find the sum of an array's elements!")
-print("Separate each element with a space like so: 1 2 3")
+print("Even works with large integers!")
+print("Separate each element with a space like so: 1000001 1000002")
 
 # Ask for input
 ar = input("Enter the array: ")
