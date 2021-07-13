61a40fb8dbd1ea8122a0756ea168b4bc05a6042d
baylierh <higginbothamb@DESKTOP-60TK8HU>
2021-07-13 15:51:58 -0400

    Code should produce the correct output now

--- 009python-tuples/code/python-tuples.py
+++ 009python-tuples/code/python-tuples.py
@@ -2,8 +2,20 @@
 #  input, create a tuple, t, of those  integers.
 # Then compute and print the result of hash(t).
 
+# fixArrayInput takes an array that is in string form
+# (ex: 1 2 3) and turns it into a list of integers
+def fixArrayInput(ar):
+    new_ar = []
+    for i in ar.split():
+        new_ar.append(int(i))
+    return new_ar
+
 integer_list = input("Enter list of integers separated with spaces: ")
 
-t = tuple(integer_list)
+fixed_list = fixArrayInput(integer_list)
+print(fixed_list)
+
+t = tuple(fixed_list)
+print(t)
 
 print(hash(t))
