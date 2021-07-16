0d495735bc8bbcbe59d5b4f0dc8e8a97c87a6a66
baylierh <higginbothamb@DESKTOP-60TK8HU>
2021-07-16 13:03:40 -0400

    Added rounding feature to average function

--- 011py-introduction-to-sets/code/py-introduction-to-sets.py
+++ 011py-introduction-to-sets/code/py-introduction-to-sets.py
@@ -8,7 +8,8 @@
 def average(arr):
     set_arr = set(arr)
     avg = sum(set_arr) / len(set_arr)
-    return avg
+    rounded = round(avg, 3)
+    return rounded
 
 # From a previous code...
 # fixArrayInput takes an array that is in string form
@@ -19,8 +20,13 @@ def fixArrayInput(ar):          #ar --> "1 2"
         new_ar.append(int(i))   
     return new_ar               #new_ar --> [1, 2]
 
+# Ask for input
 n = int(input("Enter the size of the array: "))
 inp_arr = input("Enter the values in the array separated by spaces: ")
+
+# 'Fix' the input (turn into a list)
 new_arr = fixArrayInput(inp_arr)
 
-print(average(new_arr))
+# Find the average and print it out!
+ans = average(new_arr)
+print(ans)
