14b0e3c123297f62a571b0817d37f1770e9a5144
baylierh <higginbothamb@DESKTOP-60TK8HU>
2021-07-16 12:26:52 -0400

    Edited and added comments

--- 010list-comprehensions/code/list-comprehensions.py
+++ 010list-comprehensions/code/list-comprehensions.py
@@ -6,26 +6,26 @@
 # Here, 0<i<x; 0<j<y; 0<k<z. Please use list comprehensions
 #  rather than multiple loops, as a learning exercise.
 
+# Ask for x, y, z, and n
 x = int(input("x: "))
 y = int(input("y: "))
 z = int(input("z: "))
 n = int(input("n: "))
 
-lst = [x, y, z]
-
+# Make a list of the possible x's, y's, and z's
+# Ex: if x is 2, xs will be [0, 1, 2]
 xs = [i for i in range(x+1) if x >= 0]
-print(xs)
+#print(xs)
 
 ys = [i for i in range(y+1) if y >= 0]
-print(ys)
+#print(ys)
 
 zs = [i for i in range(z+1) if z >= 0]
-print(zs)
+#print(zs)
 
 perm = [[i, j, k]   for i in xs
                     for j in ys
-                    for k in zs]
-                
+                    for k in zs]             
 #print(perm)
 
 dont_sum_to_n = [i for i in perm 
