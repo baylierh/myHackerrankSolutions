859878aef75db62d8bad45aa48ba47d44cc42102
baylierh <higginbothamb@DESKTOP-60TK8HU>
2021-07-18 19:01:23 -0400

    Now takes input and returns average

--- 012finding-the-percentage/code/finding-the-percentage.py
+++ 012finding-the-percentage/code/finding-the-percentage.py
@@ -3,6 +3,13 @@
 #  a list of students. Print the average of the marks 
 #  array for the student name provided, with 2 decimal places.
 
+# average() function is from py-introduction-to-sets.py
+def average(arr):
+    set_arr = set(arr)
+    avg = sum(set_arr) / len(set_arr)
+    rounded = round(avg, 2)
+    return rounded
+
 n = int(input("Enter n: "))
 student_marks = {}
 
@@ -11,9 +18,13 @@ for i in range(n):
     name, scores = line[0], line[1:]
     scores = list(map(float, scores))
     student_marks[name] = scores
-    print(name, scores)
+    #print(name, scores)
 
 
-print(student_marks)
+query_name = input("Enter a name: ")
 
-#query_name = input("Enter a name: ")
+student_avg = average(student_marks[query_name])
+
+print(student_avg)
+#print("{:.2f}".format(student_avg)) 
+# ^Format answer so that it passes on hackerrank
