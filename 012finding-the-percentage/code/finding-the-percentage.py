3d97d01243342afd0de1040add9b284b48c98913
baylierh <higginbothamb@DESKTOP-60TK8HU>
2021-07-18 19:08:38 -0400

    Cleaned up and added comments

--- 012finding-the-percentage/code/finding-the-percentage.py
+++ 012finding-the-percentage/code/finding-the-percentage.py
@@ -10,9 +10,13 @@ def average(arr):
     rounded = round(avg, 2)
     return rounded
 
+# Ask how many students will be entered
 n = int(input("Enter n: "))
+
+# Create a dictionary
 student_marks = {}
 
+# Ask for input and save it to a dictionary
 for i in range(n):
     line = input("Name and marks of the student: ").split()
     name, scores = line[0], line[1:]
@@ -20,11 +24,14 @@ for i in range(n):
     student_marks[name] = scores
     #print(name, scores)
 
-
+# Ask for a specific student's name
 query_name = input("Enter a name: ")
 
+# Find the average of that student's marks by 'searching'
+#  their name in the student_marks dictionary
 student_avg = average(student_marks[query_name])
 
-print(student_avg)
+# Print it out
+print(query_name, "'s average: ", student_avg, sep ='')
 #print("{:.2f}".format(student_avg)) 
 # ^Format answer so that it passes on hackerrank
