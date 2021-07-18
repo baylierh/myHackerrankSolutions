04214a00995b31cccab8836ff279374fbbe9be65
baylierh <higginbothamb@DESKTOP-60TK8HU>
2021-07-18 19:31:26 -0400

    Edited method of printing

--- 013whats-your-name/code/whats-your-name.py
+++ 013whats-your-name/code/whats-your-name.py
@@ -7,9 +7,9 @@
 #
 
 def print_full_name(first, last):
-    print("Hello ", first," ", last, "! You just delved into python.", sep='')
+    print("Hello " + first + " " + last + "! You just delved into python.")
 
 if __name__ == '__main__':
-    first_name = input()
-    last_name = input()
+    first_name = input("Enter your first name: ")
+    last_name = input("Enter your last name: ")
     print_full_name(first_name, last_name)
