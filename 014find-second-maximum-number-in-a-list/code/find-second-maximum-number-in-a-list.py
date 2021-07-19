74bffe14cc037a07d58dac2f6269b17c4f5d3145
baylierh <higginbothamb@DESKTOP-60TK8HU>
2021-07-18 21:00:27 -0400

    Added comments and finishing touches

--- 014find-second-maximum-number-in-a-list/code/find-second-maximum-number-in-a-list.py
+++ 014find-second-maximum-number-in-a-list/code/find-second-maximum-number-in-a-list.py
@@ -3,17 +3,24 @@
 # Store them in a list and find the score of the runner-up.
 
 if __name__ == '__main__':
+    # Ask for the number of scores
     n = int(input())
+    # Ask for the scores separated by spaces
     arr = list(map(int, input().split()))
-    print(arr)
+    #print(arr)
+    # Intialize the variables first and second
     first = 0
     second = 0
+    # Make a loop that will cycle through the list
+    #  to fin the highest score
     for i in range(n):
         if arr[i] > first:
             first = arr[i]
+    # Make a loop that will find the second highest score
     for i in range(n):
         if arr[i] < first:
             if arr[i] > second:
                 second = arr[i]
-    print(first)
-    print(second)
+    #print(first)
+    # Print out the second highest score!
+    print("The second highest score is...", second)
