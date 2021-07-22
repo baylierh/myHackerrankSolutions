2a3dea382b690cd8232dbe13cbd334c065c05356
baylierh <baylierh@laptop>
2021-07-21 22:19:23 -0400

    Added comments

--- 016swap-case/code/swap-case.py
+++ 016swap-case/code/swap-case.py
@@ -2,17 +2,20 @@
 # Convert all lowercase letters to uppercase letters and vice versa.
 
 def swap_case(s):
-    inv = ""
+    inv = ""                        #create an empty string
     for i in s:
-        if i.isupper() == True:
-            inv += i.lower()
-        elif i.islower() == True:
-            inv += i.upper()
+        if i.isupper() == True:     #if the ith letter in s is uppercase
+            inv += i.lower()        #add the lowercase version of it to inv
+        elif i.islower() == True:   #if the ith letter in s is lowercase
+            inv += i.upper()        #add the uppercase version of it to inv
         else:
-            inv += i
+            inv += i                #add the ith element to inv if it isn't a letter
     return inv
 
 if __name__ == '__main__':
+    # Ask for input
     s = input()
+    # Swap the cases!
     result = swap_case(s)
+    # Print it out
     print(result)
