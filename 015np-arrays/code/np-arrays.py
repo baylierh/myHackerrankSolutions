5e44df2798a109c393146c8fc2532e9b13a7160a
baylierh <baylierh@laptop>
2021-07-21 21:19:09 -0400

    Comments added

--- 015np-arrays/code/np-arrays.py
+++ 015np-arrays/code/np-arrays.py
@@ -2,13 +2,19 @@
 # Your task is to print a reversed NumPy array with 
 #  the element type float.
 
+# Import numpy
 import numpy as np
 
+# Creat the array() function that takes a list 
+#  with space separated entries
 def arrays(arr):
-    arr.reverse()
-    new_arr = np.array(arr, float)
+    arr.reverse()                   #reverse the given list
+    new_arr = np.array(arr, float)  #create new float np array
     return new_arr
 
+# Ask for input
 arr = input("Enter you array with each entry separated by spaces: ").strip().split(' ')
+
+# Let arrays() return the reverse float version of the list
 result = arrays(arr)
 print(result)
