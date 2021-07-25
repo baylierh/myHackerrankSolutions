671f1a4a7ae2a227ed505cc0aca38c62fe758cd0
baylierh <baylierh@laptop>
2021-07-25 12:56:13 -0400

    split_and_join() works

--- 017python-string-split-and-join/code/python-string-split-and-join.py
+++ 017python-string-split-and-join/code/python-string-split-and-join.py
@@ -2,9 +2,18 @@
 #  delimiter and join using a - hyphen.
 
 def split_and_join(line):
-    # code
+    line = line.split(" ")
+    new = ""
+    count = 0
+    for i in line:
+        if count != len(line) - 1:
+            new = new + i + "-"
+            count += 1
+        else:
+            new = new + i
+    return new
 
 if __name__ == '__main__':
     line = input()
     result = split_and_join(line)
-    print result
+    print(result)
