21d5be50f63fc87fd439b2d731fc98146348e24c
baylierh <baylierh@laptop>
2021-07-25 12:33:21 -0400

    basic code and instructions from hackkerank

---
+++ 017python-string-split-and-join/code/python-string-split-and-join.py
@@ -0,0 +1,10 @@
+# You are given a string. Split the string on a " " (space)
+#  delimiter and join using a - hyphen.
+
+def split_and_join(line):
+    # code
+
+if __name__ == '__main__':
+    line = input()
+    result = split_and_join(line)
+    print result
